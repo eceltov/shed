2e2be5d7d3e065efc08e6f525b3cf4f4157357f0
Vojtech Kloda <vojtech.kloda@gmail.com>
2021-12-03 08:27:14 +0000

    controller update

--- dev/controller/Controller.js
+++ dev/controller/Controller.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const LoadBalancer = require("./LoadBalancer");
 var fs = require('fs');
+var reactViews = require('express-react-views');
+/*var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');*/
 
 class Controller {
     constructor() {
@@ -20,14 +25,62 @@ class Controller {
     }
 
     initializeHttpServer() {
+        /// This example comes from the following source:
+        /// https://github.com/reactjs/express-react-views
         this.app = express();
+        let app = this.app;
+        app.set('views', __dirname + "/views");
+        app.set('view engine', 'js');
+        app.engine('js', reactViews.createEngine());
+
+        app.use(express.static(__dirname + '/public'));
+
+        app.get('/', function(req, res) {
+        var initialState = {
+            items: [
+            'document your code',
+            'drop the kids off at the pool',
+            '</script><script>alert(666)</script>',
+            ],
+            text: '',
+        };
+        res.render('Html.js', {data: initialState});
+        });
+
+        app.listen(8060, function() {
+        console.log('Dynamic react example listening on port ' + 8060);
+        });
+
+        /* Simple react rendering
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jsx');
+        app.engine('jsx', reactViews.createEngine());
+        app.use(express.static(path.join(__dirname, 'public')));
+
+
+        app.locals.something = 'value';
+        app.locals.qaz = 'qut';
+
+        app.get('/', routes.index);
+        app.get('/users', user.list);
+
+        http.createServer(app).listen(8060, function() {
+        console.log('Express server listening on port ' + 8060);
+        });*/
+
+
+        /* Initial file display
         this.app.listen(this.port, () => {
             console.log("Application started and Listening on port " + this.port);
         });
         
         this.app.get("/", (req, res) => {
-            res.send("Hello world!");
+            res.redirect("/something");
         });
+
+        this.app.get("/something", (req, res) => {
+            res.render("controller.html", { data: 5 });
+        });*/
     }
 }
 
--- dev/controller/LoadBalancer.js
+++ dev/controller/LoadBalancer.js
@@ -1,12 +1,28 @@
 class LoadBalancer {
     constructor() {
         //this.clientMessageProcessor = this.clientMessageProcessor.bind(this);
-        this.serverPorts = [];
+        this.port = null;
+        this.serverURLs = [];
     }
 
     initialize(config) {
-        this.serverPorts = config.serverPorts;
+        this.port = config.controllerPort;
+        this.serverURLs = config.serverURLs;
     }
+
+    /**
+     * @returns Returns the URL of a document server that has the smallest work load,
+     *          or null if none available.
+     * @note ///TODO: not fully implemented
+     */
+    getDocServerURL() {
+        if (this.serverURLs.length === 0) {
+            return null;
+        }
+
+        return this.serverURLs[0];
+    }
+
 }
 
 module.exports = LoadBalancer;
