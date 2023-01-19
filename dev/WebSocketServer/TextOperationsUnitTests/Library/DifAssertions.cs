513ad4fa7a8256d97076b147e1cc6f08c2543c55
Vojtech Kloda <vojtech.kloda@iguassu.cz>
2023-01-19 10:48:49 +0100

    moved projects to different folder, created unit test project

---
+++ dev/WebSocketServer/TextOperationsUnitTests/Library/DifAssertions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextOperations.Types;
+
+namespace TextOperationsUnitTests.Library
+{
+    internal class DifAssertions
+    {
+        public void TestLIT(string testName, Dif dif)
+        {
+
+        }
+    }
+}
---
+++ dev/WebSocketServer/TextOperationsUnitTests/Usings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Dif = System.Collections.Generic.List<TextOperations.Types.Subdif>;
+global using WrappedDif = System.Collections.Generic.List<TextOperations.Types.SubdifWrap>;
